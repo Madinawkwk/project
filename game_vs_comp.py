09741b025957c79fea65f7748a6a8f051f10db57
Madinawkwk <msh.ajj@bk.ru>
2024-10-01 19:29:51 +0300

    Update game_vs_comp.py

--- game_vs_comp.py
+++ game_vs_comp.py
@@ -6,60 +6,58 @@
 ##9--20%
 import random
 
-def uspex(pr1):
-    return random.random() < pr1
+def success(f):
+    pr = 1 - (f - 1) / 10
+    return random.random() < pr
 
-def uspex(pr2):
-    return random.random() < pr2
 
-def is_valid(f1):
-    return str(f1).isdigit() and 1<=f1<=9
+def is_valid(f):
+    return 1 <= f <= 9
+
 
 def play():
     hp1, hp2 = 50, 50
-    print ('Отлично! Давайте начнем!') 
-    while hp1>=0 and hp2>=0:
-        print ('Введите Вашу силу f, 1<=f<=9')
-        f1 = int(input())
-        while is_valid(f1)!= True:
-            print ('Меня не обманешь. Введите Вашу силу f, 1<=f<=9')
-            f1 = int(input())
+    print("Great! Let's get started!") 
+    while hp1 >= 0 and hp2 >= 0:
+        print('Enter your f strength, 1<=f<=9')
+        f = int(input())
+        while not is_valid(f):
+            print("You can't fool me. Enter your f strength, 1<=f<=9")
+            f = int(input())
         
-        pr1 = (100-10*(f1-1))/100
-        if uspex(pr1)==True:
-            hp2-=f1
-            print ('Поздравляю! Вы попали в цель!')
-            print (hp1, '- Ваше здоровье;', hp2, '- Здоровье Вашего противника')
-            print ()
+        if success(f)==True:
+            hp2 -= f
+            print("Congratulations! You've hit the target!")
+            print(hp1, '- Your health;', hp2, "- Computer's Health")
+            print()
         else:
-            print ('Увы, Вы не смогли попасть в цель!')
-            print (hp1, '- Ваше здоровье;', hp2, '- Здоровье Вашего противника')
-            print ()
+            print("Alas, you couldn't hit the target!")
+            print(hp1, '- Your health;', hp2, "- Computer's Health")
+            print()
         if hp2<=0:
-            print ('Поздравляю Вы выиграли')
+            print('Congratulations you have won')
             break
 
-        f2 = random.randint(1, 9)
-        print ('Сила компьтера f равна', f2)
-        pr2 = (100-10*(f2-1))/100
-        if uspex(pr2)==True:
-            hp1-=f2
-            print ('Компьютер попал в цель')
-            print (hp1, '- Ваше здоровье;', hp2, '- Здоровье компьютера')
-            print ()
+        f = random.randint(1, 9)
+        print('The power of the computer f is equal to', f)
+        if success(f)==True:
+            hp1 -= f
+            print('The computer hit the target')
+            print(hp1, '- Your health;', hp2, "- Computer's Health")
+            print()
         else:
-            print ('Компьютер не попал в цель')
-            print ()
+            print('The computer missed its target')
+            print()
         if hp1<=0:
-            print ('Увы, Вы проиграли')
+            print('Alas, you have lost')
             break
 
-ans = input('Хотите сыграть в игру? (д=да, н=нет)')
-print (ans)
-if ans.lower()=='д' or ans.lower()=='l':
+ans = input('Do you want to play a game? (y=yes, n=no)')
+print(ans)
+if ans.lower()=='y' or ans.lower()=='н':
     play()
 else:
-    print ('Хорошо, приходите позже!')
+    print('Okay, come back later!')
 
 
 
